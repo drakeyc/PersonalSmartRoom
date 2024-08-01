83c7766e2156ed79d6f4171eaa51fcd8d2fc21c6
crafturrrr <43547286+drakeyc@users.noreply.github.com>
2024-07-31 22:43:23 -0400

    CreateUser.py
    Create User and add the user to the cloud (wia) - the access token for the wia cloud is hardcoded into wia.access_token = "accesstoken"

---
+++ CreateUser.py
@@ -0,0 +1,18 @@
+def createUser():
+  camera = PiCamera()
+  camera.resolution = (1000,800)
+  camera.start_preview()
+  time.sleep(2)
+  fileName = str(uuid.uuid4())+".jpeg" camera.capture('/home/pi/Desktop/'+fileName) camera.stop_preview()
+  camera.close()
+  wia = Wia()
+  wia.access_token = "d_sk_Dku9mWR5BvmqGBu4DlVbleEZ"
+  try:
+    wia.Event.publish(name = "user",file = '/home/pi/Desktop/'+fileName) print("")
+    print("Image uploading...")
+    time.sleep(1)
+    print("")
+    print("Image uploaded!")
+    print("")
+  except Exception as error: 
+    print(error)
